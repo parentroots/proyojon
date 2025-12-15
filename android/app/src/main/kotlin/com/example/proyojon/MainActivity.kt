ac3e96767670537bd60238096308eebfbb05a868
ebrahimnazmul <ebrahimnazmul20032@gmail.com>
2025-12-15 20:51:21 +0600

    Initial commited

---
+++ android/app/src/main/kotlin/com/example/proyojon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyojon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/constant/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color primaryBlue=Color(0xFF40BFFF);
+  static const Color primaryRead=Color(0xFFFB7181);
+  static const Color primaryYellow=Color(0xFFFFC833);
+  static const Color primaryGreen=Color(0xFF53D1B6);
+  static const Color purple=Color(0xFF5C61F4);
+  static const Color primaryDark=Color(0xFF40BFFF);
+  static const Color primaryGrey=Color(0xFFFB7181);
+  static const Color primaryLight=Color(0xFFFFC833);
+  static const Color white=Color(0xFF40BFFF);
+
+}
